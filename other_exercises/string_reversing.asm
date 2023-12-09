26641002e2c2c75f6b6e272580f5fcf707432b4b
Lars Friese <57333885+larsfriese@users.noreply.github.com>
2023-12-09 23:33:26 +0100

    Removed unnecessary logic.

--- other_exercises/string_reversing.asm
+++ other_exercises/string_reversing.asm
@@ -25,37 +25,35 @@ rev_str:	.asciiz "Reversed string: \t"
 	li $v0, 8
 	syscall
 	
-	# get string length first
-	# string stored at start of data segment
+	
+	# get length of string
 	la $20, segment1
-	# $21 reserved for length
-next:
+	
+count_loop:
 	lb $22, 0($20)
-	beq $22, $zero, loop
-	addi $20, $20, 1 # memory adress
-	addi $21, $21, 1 # counter
-	j next
+	beq $22, $zero, copy_loop_start
+	addi $20, $20, 1 # increment memory adress
+	j count_loop
+	
+# $20 now contains the adress of the ending of the inputed string
 
-loop:
-	subi $21, $21, 2
-	la $20, segment1
-	add $20, $20, $21
+copy_loop_start:
+	subi $20, $20, 2 # subtract two so that enter and newline symbol are not copied
+	la $23, segment2 # load start of segment 2 into $23
 	
-	la $23, segment2
-	
-loopus:
+copy_loop:
 	
 	lb $22, 0($20) # load char from segment 1
 	sb $22, 0($23) # save char to segment 2
 	
-	beqz $21, done # if counter == 0
-	
 	addi $23, $23, 1 # adress segment 2 += 1
 	subi $20, $20, 1 # adress segment1 -= 1
-	subi $21, $21, 1 # counter -= 1
 	
+	li $24, 0x1000FFFF
+	beq $20, $24, done # if adress is 1 before start of data segment, then end copying
 	
-	j loopus
+	j copy_loop
+	
 done:
 	la $a0, rev_str
 	li $v0, 4
@@ -64,10 +62,7 @@ done:
 	la $a0, segment2
 	li $v0, 4
 	syscall
-	
-	j exit
 
-exit:
 	# clean exit
 	li $v0, 10
 	syscall
